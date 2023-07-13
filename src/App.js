5bed952d6d94e77c2e21bcf1f52d93936bdafe44
Ayeza <ayezabashir442@gmail.com>
2023-07-13 23:55:52 +0500

    installing codemirror v5 and react-codemirror2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import Codeeditor from "./components/Codeeditor";
+
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <div className="pane top-pane">
+        <Codeeditor />
+        <Codeeditor />
+        <Codeeditor />
+
+      </div>
+      <div className="pane bottom-pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Codeeditor.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/css/css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/xml/xml'
+
+const Codeeditor = () => {
+    return (
+        <div>
+            codeeditor
+        </div>
+    )
+}
+
+export default Codeeditor
